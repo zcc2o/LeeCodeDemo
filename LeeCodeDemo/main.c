b4f124a2b41415271ded518016bbdb90dbd510eb
zccmacmini <q821386496@qq.com>
2020-05-12 16:24:47 +0800

    Initial Commit

---
+++ LeeCodeDemo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LeeCodeDemo
+//
+//  Created by Laibu tech_2 on 2020/5/12.
+//  Copyright © 2020 Laibu tech_2. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
